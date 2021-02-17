c554d5ac47a4c7b19af0ed240e137b00621b6589
mrsujeet96 <mrsujeet96@gmail.com>
2021-02-17 09:57:39 +0530

    Hello World Web Aplication

---
+++ restful-web-services/src/main/java/com/sujeet/rest/webservices/helloworld/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.sujeet.rest.webservices.helloworld;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloWorldController {
+
+@GetMapping( path = "/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+
+    @GetMapping( path = "/hello-world-bean")
+    public HelloWorldBean helloWorldbean(){
+        return new  HelloWorldBean("Hello World");
+    }
+   ///hello-world/path-variable/sujeet
+    @GetMapping( path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new  HelloWorldBean(String.format("Hello World %s" ,name));
+    }
+}
---
+++ restful-web-services/src/main/java/com/sujeet/rest/webservices/user/User.java
@@ -0,0 +1,4 @@
+package com.sujeet.rest.webservices.user;
+
+public class User {
+}
