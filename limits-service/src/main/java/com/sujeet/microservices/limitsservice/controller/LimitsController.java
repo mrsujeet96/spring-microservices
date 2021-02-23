3149ebeb5a58669ea2866c88e62f8675fe945d73
mrsujeet96 <mrsujeet96@gmail.com>
2021-02-23 20:07:27 +0530

    First Commit

---
+++ limits-service/src/main/java/com/sujeet/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,19 @@
+package com.sujeet.microservices.limitsservice.controller;
+
+
+import com.sujeet.microservices.limitsservice.bean.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits(){
+        return new Limits(1,1000);
+
+    }
+
+
+
+}
