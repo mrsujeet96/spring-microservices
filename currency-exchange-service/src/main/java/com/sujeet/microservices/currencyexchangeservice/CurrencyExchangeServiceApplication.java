5250614532c1b48b96df4ca17353da6b18c9b951
mrsujeet96 <mrsujeet96@gmail.com>
2021-03-03 11:01:57 +0530

    updating

--- currency-exchange-service/src/main/java/com/sujeet/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/sujeet/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,6 +2,8 @@ package com.sujeet.microservices.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 public class CurrencyExchangeServiceApplication {
